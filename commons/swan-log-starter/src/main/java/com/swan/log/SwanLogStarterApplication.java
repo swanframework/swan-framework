a6428eb0c3c0d5243431501111935f77732eba7c
zonggaofeng <zonggaofeng@bytedance.com>
2022-11-06 17:07:55 +0800

    Initial commit

---
+++ commons/swan-log-starter/src/main/java/com/swan/log/SwanLogStarterApplication.java
@@ -0,0 +1,13 @@
+package com.swan.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwanLogStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwanLogStarterApplication.class, args);
+    }
+
+}
---
+++ commons/swan-log-starter/src/test/java/com/swan/log/SwanLogStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swan.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwanLogStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
