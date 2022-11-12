c84c610b3c490acc76f53618c79f9d5bc6dac4b3
zonggaofeng <zonggaofeng@bytedance.com>
2022-11-12 12:57:08 +0800

    新增注释

--- starters/swan-mybatis-starter/src/main/java/com/swan/mybatis/mapper/methods/CountMethod.java
+++ starters/swan-mybatis-starter/src/main/java/com/swan/mybatis/mapper/methods/CountMethod.java
@@ -2,6 +2,11 @@ package com.swan.mybatis.mapper.methods;
 
 public interface CountMethod<ID, E, C>  extends BaseMethod {
 
+    /** 统计表中未删除的记录总数 <br/>
+     *  * 如果删除策略为物理删除，则总数为表中实际数量 <br/>
+     *  * 如果删除策略为伪删除，则总数为使用 @Delete 修饰字段等于 @Delete.no 的记录总数 <br/>
+     * @return Integer
+     */
     public Integer count();
 
 }
--- starters/swan-mybatis-starter/src/main/java/com/swan/mybatis/mapper/methods/CountOnConditionMethod.java
+++ starters/swan-mybatis-starter/src/main/java/com/swan/mybatis/mapper/methods/CountOnConditionMethod.java
@@ -1,9 +1,17 @@
 package com.swan.mybatis.mapper.methods;
 
+import com.sun.istack.internal.NotNull;
 import org.apache.ibatis.annotations.Param;
 
 public interface CountOnConditionMethod<ID, E, C>  extends BaseMethod {
 
-    public Integer countOnCondition(@Param("condition") C condition);
+    /** 根据条件获取记录总数 <br/>
+     *  * 当 condition 为 null 时，统计表中全部数据 <br/>
+     *  * 如果删除策略为物理删除，则总数为表中实际数量 <br/>
+     *  * 如果删除策略为伪删除，则总数为使用 @Delete 修饰字段等于 @Delete.no 的记录总数 <br/>
+     * @param condition 查询条件
+     * @return
+     */
+    public Integer countOnCondition(@Param("condition") @NotNull C condition);
 
 }
