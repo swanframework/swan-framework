09d0253857141a02e8f89c387e3c1da7ba3fc37c
zonggaofeng <zonggaofeng@bytedance.com>
2022-11-18 23:29:19 +0800

    更新类名

--- ztests/test-swan-mybatis-starter/src/test/java/com/swan/test/mybatis/mapper/count/AutoMapper_CountTest.java
+++ ztests/test-swan-mybatis-starter/src/test/java/com/swan/test/mybatis/mapper/count/AutoMapper_CountTest.java
@@ -7,10 +7,7 @@ import com.swan.mybatis.condition.OpType;
 import org.junit.jupiter.api.Test;
 import com.swan.test.mybatis.mapper.AutoEntityFactory;
 import com.swan.test.mybatis.BaseAutoMapperTest;
-import com.swan.test.mybatis.po.AutoDelEntity;
-import com.swan.test.mybatis.po.AutoDelVersionEntity;
 import com.swan.test.mybatis.po.AutoEntity;
-import com.swan.test.mybatis.po.AutoVersionEntity;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,8 +17,7 @@ import org.junit.jupiter.api.Assertions;
  * @author zongf
  * @date 2021-01-10
  */
-public class CountTest extends BaseAutoMapperTest {
-
+public class AutoMapper_CountTest extends BaseAutoMapperTest {
 
     @Test
     public void none_param() {
