e1f2163bb9dfbc7af4fb9885b52f9b822d905ce6
zonggaofeng <zonggaofeng@bytedance.com>
2022-11-19 23:21:44 +0800

    新增 threadPool 测试

---
+++ ztests/test-swan-core-starter/src/main/java/com/swan/test/core/TestSwanCoreStarterApplication.java
@@ -0,0 +1,13 @@
+package com.swan.test.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSwanCoreStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSwanCoreStarterApplication.class, args);
+    }
+
+}
---
+++ ztests/test-swan-core-starter/src/test/java/com/swan/test/core/threadPool/ThreadPoolTest.java
@@ -0,0 +1,32 @@
+package com.swan.test.core.threadPool;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.test.context.ActiveProfiles;
+
+@Slf4j
+@SpringBootTest
+@ActiveProfiles("threadPool")
+public class ThreadPoolTest {
+
+    @Autowired
+    private ThreadPoolTaskExecutor jobPool;
+
+    @Autowired
+    private ThreadPoolTaskExecutor taskPool;
+
+    @Test
+    public void env() {
+        Assertions.assertEquals(5, jobPool.getCorePoolSize());
+    }
+
+    @Test
+    public void taskPool() {
+        Assertions.assertEquals(10, taskPool.getCorePoolSize());
+    }
+
+}
