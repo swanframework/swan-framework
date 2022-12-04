9d49fa781316fe052af92785da93ea85a695a65e
zonggaofeng <zonggaofeng@bytedance.com>
2022-12-04 12:50:01 +0800

    提供 Length 扩展样例，后续提供更多扩展

--- starters/swan-knife4j-starter/src/main/java/com/swan/knife4j/core/CustomModelResolver.java
+++ starters/swan-knife4j-starter/src/main/java/com/swan/knife4j/core/CustomModelResolver.java
@@ -6,7 +6,10 @@ import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.media.StringSchema;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.lang.annotation.Annotation;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,9 +24,9 @@ public class CustomModelResolver extends ModelResolver {
     }
 
     @Override
-    protected void applyBeanValidatorAnnotations(Schema schema, Annotation[] annotations, Schema parent) {
+    protected void applyBeanValidatorAnnotations(Schema property, Annotation[] annotations, Schema parent) {
         // 调用父类处理逻辑
-        super.applyBeanValidatorAnnotations(schema, annotations, parent);
+        super.applyBeanValidatorAnnotations(property, annotations, parent);
 
         // 自定义扩展逻辑
         Map<String, Annotation> annos = new HashMap<>();
@@ -35,11 +38,25 @@ public class CustomModelResolver extends ModelResolver {
 
         // 兼容处理 @Length 注解
         if (annos.containsKey("org.hibernate.validator.constraints.Length")) {
-            if (schema instanceof StringSchema) {
+            if (property instanceof StringSchema) {
                 Length pattern = (Length) annos.get("org.hibernate.validator.constraints.Length");
-                StringSchema sp = (StringSchema) schema;
+                StringSchema sp = (StringSchema) property;
                 sp.minLength(Integer.valueOf(pattern.min()));
                 sp.maxLength(Integer.valueOf(pattern.max()));
+            }
+        }
+
+        // 处理 @Max 注解，包含边界值
+        if (annos.containsKey("javax.validation.constraints.Max")) {
+            if ("integer".equals(property.getType()) || "number".equals(property.getType())) {
+                property.setExclusiveMaximum(true);
+            }
+        }
+
+        // 处理 @Min 注解，包含边界值
+        if (annos.containsKey("javax.validation.constraints.Min")) {
+            if ("integer".equals(property.getType()) || "number".equals(property.getType())) {
+                property.setExclusiveMinimum(true);
             }
         }
     }
