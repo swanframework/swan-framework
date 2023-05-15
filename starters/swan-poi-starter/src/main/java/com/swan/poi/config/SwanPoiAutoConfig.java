768400019ca04aceac28df82fcc7144699d4bb52
zonggaofeng <zonggaofeng@bytedance.com>
2023-05-16 00:33:09 +0800

    新增poi 模块

---
+++ starters/swan-poi-starter/src/main/java/com/swan/poi/config/SwanPoiAutoConfig.java
@@ -0,0 +1,12 @@
+package com.swan.poi.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zongf
+ * @since 2023-05-16
+ **/
+@Configuration(proxyBeanMethods = false)
+public class SwanPoiAutoConfig {
+
+}
